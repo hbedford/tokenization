faa7a4991ad8b9f3bdd173500ed3ae50675d1b6a
hbedford <36144175+hbedford@users.noreply.github.com>
2022-06-04 19:52:23 -0300

    Initial commit

---
+++ lib/src/cores/app_colors.dart
@@ -0,0 +1,26 @@
+
+//
+// app_colors.dart
+//
+
+// Do not edit directly
+// Generated on Sat, 04 Jun 2022 22:50:04 GMT
+
+
+
+import 'dart:ui';
+
+class StyleDictionaryColor {
+  StyleDictionaryColor._();
+
+    static const backgroundInverse = Color(0xFF111111);
+    static const backgroundLink = Color(0xFF0366D6);
+    static const backgroundPrimary = Color(0xFFFFFFFF);
+    static const coreBlack = Color(0xFF111111);
+    static const coreBlue = Color(0xFF0366D6);
+    static const coreGray = Color(0xFFDDDDDD);
+    static const coreWhite = Color(0xFFFFFFFF);
+    static const fontInverse = Color(0xFFFFFFFF);
+    static const fontLink = Color(0xFF0366D6);
+    static const fontPrimary = Color(0xFF111111);
+}
